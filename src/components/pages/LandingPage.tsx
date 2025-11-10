fb217261ecb332901b6e049c224af07dc9614875
elinfelicia <elinratcliff@icloud.com>
2025-11-10 13:07:17 +0100

    remove old auth links

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -21,10 +21,6 @@ function LandingPage() {
   return (
     <div className="landing-page">
       <img src="/assets/05.png" alt="JobChaser" className="landing-page__image" />
-      <div className="landing-links">
-        <Link to="/signup" className="nav-link">Sign Up</Link>
-        <Link to="/signin" className="nav-link">Sign In</Link>
-      </div>
     </div>
   );
 }
